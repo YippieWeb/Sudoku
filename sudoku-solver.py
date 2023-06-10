74aac5f8d846772f3b86a3f8cde0df842a2962b1
Jasmine Yip <yippie.web@gmail.com>
2023-06-10 20:19:44 +1000

    Check number in row, column, and square

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -10,3 +10,27 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,0,8,0,0,0,0]]
 
 print(np.matrix(grid))
+
+def possible(row, column, number):
+    global grid 
+    # is num in given row (scan left to right)
+    for i in range(0,9):
+        if grid[row][i] == number:
+            return False
+
+    # is num in given column (scan top to bottom)
+    for i in range(0,9):
+        if grid[i][column] == number:
+            return False
+
+    # num in given square
+    x0 = (column // 3) * 3 # column 0,3,6
+    y0 = (row // 3) * 3 # row 0,3,6
+
+    for i in range(0,3): # two units leftwards
+        for j in range(0,3): # two units rightwards
+            if grid[y0+i][x0+j] == number:
+                return False
+
+    # num is not in row, column, or sq
+    return True
